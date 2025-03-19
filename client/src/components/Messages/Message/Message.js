7b3a5353ec0591a649c2b013a470667aa8f07ee4
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2025-03-19 17:13:44 +0530

    Update Message.js

--- client/src/components/Messages/Message/Message.js
+++ client/src/components/Messages/Message/Message.js
@@ -18,8 +18,8 @@ const Message = ({ message: { text, user }, name }) => {
       ? (
         <div className="messageContainer justifyEnd">
     {/*<p className="sentText pr-10">{trimmedName}</p> */}
-          <div className="messageBox backgroundBlue">
-    <p className="sentText">{trimmedName}</p>
+          <div className="messageBox2 backgroundBlue">
+            <p className="sentText">{trimmedName}</p>
             <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
           </div>
         </div>
@@ -27,7 +27,7 @@ const Message = ({ message: { text, user }, name }) => {
         : (
           <div className="messageContainer justifyStart">
             <div className="messageBox backgroundLight">
-              <p className="sentText pl-10 ">{user}</p>
+              <p className="sentText ">{user}</p>
               <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
             </div>
 {/* <p className="sentText pl-10 ">{user}</p> */}
