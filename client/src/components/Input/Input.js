ef48872e847530b0a8b5a119b546b3067ea2b5ac
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2025-05-02 17:11:53 +0530

    Update Input.js

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -2,18 +2,55 @@ import React from 'react';
 
 import './Input.css';
 
-const Input = ({ setMessage, sendMessage, message }) => (
+const Input = ({ setMessage, sendMessage, message, handleImageChange, imageFile }) => (
+  
+  // <form className="form">
+  //   <input
+  //     className="input"
+  //     type="text"
+
+  //     placeholder="Type a message..."
+  //     value={message}
+  //     onChange={({ target: { value } }) => setMessage(value)}
+  //     onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+  //   />
+  //     <input type="file" accept="image/*" onChange={handleImageChange} />
+
+  //   <button className="sendButton" onClick={e => sendMessage(e)}>Send</button>
+  // </form>
   <form className="form">
-    <input
-      className="input"
-      type="text"
-      placeholder="Type a message..."
-      value={message}
-      onChange={({ target: { value } }) => setMessage(value)}
-      onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
-    />
-    <button className="sendButton" onClick={e => sendMessage(e)}>Send</button>
-  </form>
+  <input
+    className="input"
+    type="text"
+    placeholder="Type a message..."
+    value={message}
+    onChange={({ target: { value } }) => setMessage(value)}
+    onKeyPress={(event) => event.key === 'Enter' ? sendMessage(event) : null}
+  />
+
+  {/* Show selected file name if exists */}
+  {imageFile && (
+    <span className="fileName">{imageFile.name}</span>
+  )}
+
+  {/* Hidden file input */}
+  <input
+    type="file"
+    id="fileInput"
+    accept="image/*"
+    onChange={handleImageChange}
+    style={{ display: 'none' }}
+  />
+
+  {/* Label acts as the upload button */}
+  <label htmlFor="fileInput" className="uploadIcon">
+    +
+  </label>
+
+  <button className="sendButton" onClick={e => sendMessage(e)}>Send</button>
+</form>
+
+
 )
 
 export default Input;
