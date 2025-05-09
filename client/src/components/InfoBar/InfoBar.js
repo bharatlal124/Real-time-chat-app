c029f346d6122da90b2adf64a709e191b330ee9e
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2025-05-09 18:07:29 +0530

    Update InfoBar.js

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -5,28 +5,28 @@ import closeIcon from '../../icons/closeIcon.png';
 
 import './InfoBar.css';
 
-// const InfoBar = ({ room }) => (
-//   <div className="infoBar">
-//     <div className="leftInnerContainer">
-//       <img className="onlineIcon" src={onlineIcon} alt="online icon" />
-//       <h3>{room}</h3>
-//     </div>
-//     <div className="rightInnerContainer">
-//       <a href="/"><img src={closeIcon} alt="close icon" /></a>
-//     </div>
-//   </div>
-// );
-const InfoBar = ({ room, typingUser }) => (
+const InfoBar = ({ room }) => (
   <div className="infoBar">
     <div className="leftInnerContainer">
-    <img className="onlineIcon" src={onlineIcon} alt="online icon" />
+      <img className="onlineIcon" src={onlineIcon} alt="online icon" />
       <h3>{room}</h3>
     </div>
     <div className="rightInnerContainer">
-    <a href="/"><img src={closeIcon} alt="close icon" /></a>
-      {typingUser && <p className="typing-indicator">{typingUser} is typing...</p>}
+      <a href="/"><img src={closeIcon} alt="close icon" /></a>
     </div>
   </div>
 );
+// const InfoBar = ({ room, typingUser }) => (
+//   <div className="infoBar">
+//     <div className="leftInnerContainer">
+//     <img className="onlineIcon" src={onlineIcon} alt="online icon" />
+//       <h3>{room}</h3>
+//     </div>
+//     <div className="rightInnerContainer">
+//     <a href="/"><img src={closeIcon} alt="close icon" /></a>
+//       {typingUser && <p className="typing-indicator">{typingUser} is typing...</p>}
+//     </div>
+//   </div>
+// );
 
 export default InfoBar;
